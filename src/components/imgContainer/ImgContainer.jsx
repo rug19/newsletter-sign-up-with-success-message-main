15aae0930913bba7d39153757d78a89d609ab4b7
Ruan <rugcosta1234@gmail.com>
2024-06-26 20:13:08 -0300

    Atualizando css do component img

--- src/components/imgContainer/ImgContainer.jsx
+++ src/components/imgContainer/ImgContainer.jsx
@@ -2,8 +2,8 @@ import './imgContainer.css';
 
 export default function ImgContainer(){
     return (
-        <section>
-            <img className='img_container' src={`${import.meta.env.VITE_PUBLIC_URL}illustration-sign-up-desktop.svg`} alt="Icon List" />
+        <section className='img_container'>
+            <img  className='img_main' src={`${import.meta.env.VITE_PUBLIC_URL}illustration-sign-up-desktop.svg`} alt="Icon List" />
         </section>
     );
 }
